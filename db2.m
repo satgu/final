ad3a4ef86a4e3e3944c33baffffb9fd9ac4acc68
satgu <30535145+satgu@users.noreply.github.com>
2017-09-08 02:09:29 +0530

    Delete db2.m

--- db2.m
+++
@@ -1,4 +0,0 @@
-javaaddpath 'mysql-connector-java-5.1.6-bin.jar';
-
-conn=database('face_recognition', 'root', '', 'com.mysql.jdbc.Driver', 'jdbc:mysql://localhost/');
-e = exec(conn,'SELECT * FROM boylee');
